16a8e3f7edff1fa14d3d45affffd87d5e732bbc3
Fazal <mohamedhfazal@gmail.com>
2020-06-22 17:58:02 +0500

    working initial implementation

---
+++ watchd/scanner.py
@@ -0,0 +1,330 @@
+import magic # https://github.com/ahupp/python-magic
+import binascii
+import hashlib
+import sys
+import zipfile
+import threading
+import os
+import queue
+
+from io import BytesIO as DataIO
+
+from .zipdump import ZIPDump
+from .zipdump import YARACompile
+from .zipdump import cIdentity
+from .zipdump import LoadDecoders
+from .zipdump import QUOTE, C2BIP3
+from .zipdump import DecideToSelect
+
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Result:
+    zipfile: bool = False
+    zipentry: str = ""
+    filename: str = ""
+    decoder: str = ""
+    namespace: str = ""
+    rule: str = ""
+    sha2sum: str = ""
+    md5sum: str = ""
+    ctime: int = 0
+    mtime: int = 0
+    strings: List[str] = field(default_factory=list)
+
+
+ZIP_MIME = "application/zip"
+
+
+def remove(file):
+    try:
+        os.remove(file)
+    except Exception as e:
+        print("caught {}".format(e))
+
+def cleanempty(root):
+    """
+    This does not remove directoires recursively 
+    Therefore it won't remove all empty directories 
+    on the first run, continuous calls will eventually remove
+    all directories
+    """
+    emptydirs = []
+    for dpath, sub, fnames in os.walk(root):
+        if len(fnames) == 0 and len(sub) == 0 and dpath != root:
+            emptydirs.append(dpath)
+
+    for d in emptydirs:
+        try:
+            os.rmdir(os.path.abspath(os.path.join(root, d)))
+        except FileNotFoundError:
+            continue
+
+def scan(f, options):
+    try:
+        t = magic.from_file(f, mime=True)
+        if t == ZIP_MIME:
+            sresult = scanzip(f, options)
+            return result(f, sresult, options)
+        elif options.yara != None:
+            sresult = scanregular(f, options)
+            return result(f, sresult, options)
+    except FileNotFoundError:
+        print("{} file does not exist anymore".format(f))
+    except IsADirectoryError:
+        print("{} is a directory, ignored".format(f))
+
+def scanfull(root, scanq, options):
+    for dpath, _, fnames in os.walk(root):
+        for f in fnames:
+            fullpath = os.path.join(dpath, f)
+            scanq.put(fullpath)
+
+class ResultWriter(threading.Thread):
+    def __init__(self, resultq, options):
+        super().__init__()
+        self.__rq = resultq
+        self.__options = options
+        self.__stop = False
+    def run(self):
+        # write csv headers before main loop
+        headers = [
+                "filename", "md5sum", "sha2sum", "is_zipfile", "yara_rule",
+                "yara_ns", "yara_strings", "ctime"
+                ]
+
+        if not os.path.exists(self.__options.csv):
+            with open(self.__options.csv, 'w'): pass
+
+        csv = open(self.__options.csv, "a")
+        if not csv.writable():
+            raise Exception("file is not writable")
+        
+        filesize = os.path.getsize(self.__options.csv)
+
+        # if file is empty write headers
+        if filesize == 0:
+            csv.write("{}\n".format(",".join(headers)))
+
+        while True:
+            try:
+                if self.__stop:
+                    return
+                results = self.__rq.get(timeout=3)
+                if self.__options.csv != "" and results is not None:
+                    for result in results:
+                        rec = [
+                                result.filename,
+                                result.md5sum,
+                                result.sha2sum,
+                                str(result.zipfile),
+                                result.rule,
+                                result.namespace,
+                                "\"{}\"".format(",".join(result.strings)),
+                                str(result.ctime)
+                                ]
+                        csv.write("{}\n".format(",".join(rec)))
+
+                    csv.flush()
+                
+                # print("got result {}".format(result))
+            except queue.Empty:
+                continue
+    def stop(self):
+        self.__stop = True
+
+class FullScanner(threading.Thread):
+    def __init__(self, root, scanq, options):
+        super().__init__()
+        self.__sq = scanq
+        self.__options = options
+        self.__root = root
+        self.__stop = False
+
+    def run(self):
+        scanfull(self.__root, self.__sq, self.__options)
+
+
+class Scanner(threading.Thread):
+    def __init__(self, root, scanq, resultq, options):
+        super().__init__()
+        self.__sq = scanq
+        self.__rq = resultq
+        self.__options = options
+        self.__stop = False
+        self.__root = root
+    def run(self):
+        while True:
+            try:
+                if self.__stop:
+                    return
+                file = self.__sq.get(timeout=3)
+                result = scan(file, self.__options)
+                print("scanned file: {}".format(file))
+                self.__rq.put(result)
+                remove(file)
+            except queue.Empty:
+                cleanempty(self.__root)
+                continue
+
+    def stop(self):
+        self.__stop = True
+
+
+
+def gen_sum_file(f):
+    """
+    Generate sum, reading given file
+    chunk by chunk to avoid completely 
+    reading file into memory
+    """
+    cz = 8000
+    msum = hashlib.md5()
+    hsum = hashlib.sha256()
+    with open(f, 'rb') as f:
+        chunk = f.read(cz)
+        msum.update(chunk)
+        hsum.update(chunk)
+    return (msum.hexdigest(), hsum.hexdigest())
+
+
+def gen_sum(content):
+    """
+    Generate sum on content
+    """
+    md5 = hashlib.md5(content).hexdigest()
+    sha2 = hashlib.sha256(content).hexdigest()
+
+    return (md5, sha2)
+
+def result(fpath, result, options):
+    """
+    Do any post processing required on result
+    """
+    return result
+
+def fillfinfo(stat_result, scan_result):
+    """
+    Fill file information metadata on result
+    """
+    scan_result.ctime = stat_result.st_ctime
+    scan_result.mtime = stat_result.st_mtime
+
+def fmtyarastrings(strings):
+    r = []
+    for stringdata in strings:
+        r.append('%06x' % stringdata[0])
+        r.append(stringdata[1])
+        r.append(binascii.hexlify(stringdata[2]).decode("utf8"))
+        r.append(repr(stringdata[2]))
+    return r
+
+
+def scanregular(f, options):
+    rules = YARACompile(options.yara)
+
+    stat_result = os.stat(f)
+
+    file = open(f, "rb")
+    filecontent = file.read()
+    file.close()
+    global decoders
+
+
+    decoders = []
+    LoadDecoders(options.decoders, options.decoderdir, True)
+    oDecoders = [cIdentity(filecontent, None)]
+    for cDecoder in decoders:
+        try:
+            oDecoder = cDecoder(filecontent, options.decoderoptions)
+            oDecoders.append(oDecoder)
+        except Exception as e:
+            print('Error instantiating decoder: %s' % cDecoder.name)
+            if options.verbose:
+                raise e
+            return
+    results = []
+    md5sum, sha2sum = gen_sum(filecontent)
+    for oDecoder in oDecoders:
+        while oDecoder.Available():
+            for result in rules.match(data=oDecoder.Decode()):
+                r = Result()
+                r.filename = f
+                r.decoder = oDecoder.Name()
+                r.sha2sum = sha2sum
+                r.md5sum = md5sum
+                r.namespace  = result.namespace
+                r.rule = result.rule
+                r.strings = fmtyarastrings(result.strings)
+                fillfinfo(stat_result, r)
+                results.append(r)
+    return results
+
+def scanzip(f, options):
+    if options.yara == None: return
+    if not 'yara' in sys.modules:
+        print('Error: option yara requires the YARA Python module.')
+        return
+    rules = YARACompile(options.yara)
+    zipfilename = f
+    oZipfile = zipfile.ZipFile(zipfilename, 'r')
+    zippassword = options.password
+    counter = 0
+
+    stat_result = os.stat(f)
+
+    zipmsum, zipssum = gen_sum_file(zipfilename)
+
+    global decoders
+    decoders = []
+    LoadDecoders(options.decoders, options.decoderdir, True)
+
+    if not options.regular and len(oZipfile.infolist()) == 1:
+        try:
+            if oZipfile.open(oZipfile.infolist()[0], 'r', C2BIP3(zippassword)).read(2) == b'PK':
+                oZipfile2 = zipfile.ZipFile(DataIO(oZipfile.open(oZipfile.infolist()[0], 'r', C2BIP3(zippassword)).read()), 'r')
+                oZipfile.close()
+                oZipfile = oZipfile2
+        except:
+            pass
+    results = []
+    for oZipInfo in oZipfile.infolist():
+        counter += 1
+        if DecideToSelect(options.select, counter, oZipInfo.filename):
+            file = oZipfile.open(oZipInfo, 'r', C2BIP3(zippassword))
+            filecontent = file.read()
+            file.close()
+            # md5sum, sha2sum = gen_sum(filecontent)
+            # encrypted = oZipInfo.flag_bits & 1
+            # timestamp = '%04d-%02d-%02d %02d:%02d:%02d' % oZipInfo.date_time
+            oDecoders = [cIdentity(filecontent, None)]
+            for cDecoder in decoders:
+                try:
+                    oDecoder = cDecoder(filecontent, options.decoderoptions)
+                    oDecoders.append(oDecoder)
+                except Exception as e:
+                    print('Error instantiating decoder: %s' % cDecoder.name)
+                    if options.verbose:
+                        raise e
+                    return
+            for oDecoder in oDecoders:
+                while oDecoder.Available():
+                    for result in rules.match(data=oDecoder.Decode()):
+                        r = Result()
+                        r.filename = zipfilename
+
+                        r.zipfile = True
+                        # r.zipsha1 = zipssum
+                        # r.zipmd5 = zipmsum
+                       
+                        r.decoder = oDecoder.Name()
+                        r.sha2sum = zipssum
+                        r.md5sum = zipmsum
+                        r.namespace  = result.namespace
+                        r.rule = result.rule
+                        r.strings = fmtyarastrings(result.strings)
+                        fillfinfo(stat_result, r)
+                        results.append(r)
+    return results
+
---
+++ watchd/watch.py
@@ -0,0 +1,121 @@
+import sys
+import time
+import os
+import optparse
+import queue
+import threading
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+import magic # https://github.com/ahupp/python-magic
+
+from .zipdump import ZIPDump, ValidateOptions, OptionsEnvironmentVariables
+from .scanner import scan, Scanner, ResultWriter, scanfull, FullScanner
+
+# https://pythonhosted.org/watchdog/_modules/watchdog/events.html#PatternMatchingEventHandler
+class WatchdogHandler(FileSystemEventHandler):
+
+    def __init__(self, scanq, options):
+        super().__init__()
+
+        self.__options = options
+        self.__sq = scanq
+
+    def on_any_event(self, event):
+        abs_path = os.path.abspath(event.src_path)
+        print("{}".format(abs_path))
+    def on_created(self, event):
+        abs_path = os.path.abspath(event.src_path)
+        self.__sq.put(abs_path)
+
+
+class DirWatcher(threading.Thread):
+    def __init__(self, dirpath, scanq, options):
+        super().__init__()
+        self.__dirpath = dirpath
+        self.__handler = WatchdogHandler(scanq, options)
+        self.__observer = Observer()
+        self.__options = options
+        self.__scanq = scanq
+        self.__stop = False
+
+    def run(self):
+        self.__observer.schedule(self.__handler, self.__dirpath, recursive=True)
+        self.__observer.start()
+
+        try:
+            while True:
+                if self.__stop:
+                    return
+                pass
+        except KeyboardInterrupt:
+            return
+
+
+    def stop(self):
+        self.__observer.stop()
+        self.__observer.join()
+        self.__stop = True
+
+
+def Main():
+    oParser = optparse.OptionParser(usage="usage: %prog [dir] [rules]\n")
+    oParser.add_option('-s', '--select', default='', help='select index nr or name')
+    oParser.add_option('-o', '--output', help='Output to file')
+    oParser.add_option('-p', '--password', default='infected', help='The ZIP password to be used (default infected)')
+    oParser.add_option('-y', '--yara', help="YARA rule file (or directory or @file) to check files (YARA search doesn't work with -s option)")
+    oParser.add_option('-C', '--decoders', type=str, default='', help='decoders to load (separate decoders with a comma , ; @file supported)')
+    oParser.add_option('--decoderoptions', type=str, default='', help='options for the decoder')
+    oParser.add_option('-v', '--verbose', action='store_true', default=False, help='verbose output with decoder errors')
+    oParser.add_option('-r', '--regular', action='store_true', default=False, help='if the ZIP file contains a single ZIP file, handle it like a regular (non-ZIP) file')
+    oParser.add_option('--decoderdir', type=str, default='', help='directory for the decoder')
+    oParser.add_option('-f', '--find', type=str, default='', help='Find PK MAGIC sequence (use l or list for listing, number for selecting)')
+
+    oParser.add_option('--csv', type=str, default='result.csv', help='Output file for csv results')
+    (options, args) = oParser.parse_args()
+
+    if len(args) > 1:
+        oParser.print_help()
+        return
+
+    (options, args) = oParser.parse_args()
+
+    root = os.path.abspath(args[0])
+
+    resultq = queue.Queue()
+    scanq = queue.Queue()
+
+    resultwriter = ResultWriter(resultq, options)
+    resultwriter.daemon = True
+
+    scanner = Scanner(root, scanq, resultq, options)
+    resultwriter.daemon = True
+
+    fullscanner = FullScanner(root, scanq, options)
+    fullscanner.daemon = True
+
+    resultwriter.start()
+    scanner.start()
+    # do a full scan while it's been watched by watchdog for new files
+    fullscanner.start()
+
+
+    watcher = DirWatcher(root, scanq, options)
+    watcher.daemon = True
+    watcher.start()
+
+    fullscanner.join()
+
+    try:
+        watcher.join()
+        scanner.join()
+        resultwriter.join()
+    except KeyboardInterrupt:
+        watcher.stop()
+        scanner.stop()
+        resultwriter.stop()
+        print("shutting down")
+
+if __name__ == "__main__":
+    Main()
